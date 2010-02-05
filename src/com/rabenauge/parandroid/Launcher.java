1bb8934d4cc456f4eb0d83a099435409c498a610
eyebex <eyebex@threekings.tk>
2010-02-05 22:36:12 +0100

    Initial Android Project as created by Eclipse

---
+++ src/com/rabenauge/parandroid/Launcher.java
@@ -0,0 +1,13 @@
+package com.rabenauge.parandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Launcher extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
