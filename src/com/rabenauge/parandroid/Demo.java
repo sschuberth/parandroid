2ad27f1acadf4fbba6bdef215518ed068188d220
eyebex <eyebex@threekings.tk>
2010-02-06 21:55:22 +0100

    Log some OpenGL info to Eclipse's DDMS perspective

--- src/com/rabenauge/parandroid/Demo.java
+++ src/com/rabenauge/parandroid/Demo.java
@@ -4,16 +4,32 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLSurfaceView.Renderer;
 import android.opengl.GLU;
+import android.util.Log;
 import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.GL11;
 import javax.microedition.khronos.egl.EGLConfig;
 
 public class Demo extends GLSurfaceView implements Renderer {
+    private static final String TAG="ParaNdroiD";
+
     public Demo(Context context) {
         super(context);
         setRenderer(this);
     }
 
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // Get some OpenGL information.
+        Log.i(TAG, gl.glGetString(GL10.GL_VENDOR));
+        Log.i(TAG, gl.glGetString(GL10.GL_RENDERER));
+        Log.i(TAG, gl.glGetString(GL10.GL_VERSION));
+        Log.i(TAG, gl.glGetString(GL10.GL_EXTENSIONS).replace(' ', '\n'));
+        if (gl instanceof GL11) {
+            Log.i(TAG, "Implements GL11");
+        }
+        else {
+            Log.i(TAG, "Implements GL10");
+        }
+
         gl.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
     }
 
