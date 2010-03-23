945fa8813861eef3e1d7aadbbd493eabba619776
eyebex <eyebex@threekings.tk>
2010-03-23 19:25:07 +0100

    Testing allocateDirect() for byte buffers

--- src/com/rabenauge/gl/Helper.java
+++ src/com/rabenauge/gl/Helper.java
@@ -1,7 +1,7 @@
 package com.rabenauge.gl;
 
 import android.opengl.GLU;
-import java.nio.IntBuffer;
+import java.nio.ByteBuffer;
 import javax.microedition.khronos.opengles.GL10;
 
 /*
@@ -9,21 +9,26 @@ import javax.microedition.khronos.opengles.GL10;
  */
 public class Helper {
     // Vertices and texture coordinates for rendering a bitmap in order LR, LL, UL, UR.
-    private static final int w=(int)(1.0f*65536), h=(int)(1.0f*65536);
-    private static final IntBuffer v=IntBuffer.allocate(8).put(w).put(h).put(0).put(h).put(0).put(0).put(w).put(0);
+    private static final byte w=1, h=1;
+    private static final ByteBuffer v=ByteBuffer.allocateDirect(8).put(w).put(h).put((byte)0).put(h).put((byte)0).put((byte)0).put(w).put((byte)0);
+
+    static {
+        // Reset the buffer position before using the buffer!
+        v.position(0);
+    }
 
     public static void drawScreenSpaceQuad(GL10 gl) {
         gl.glMatrixMode(GL10.GL_PROJECTION);
         gl.glLoadIdentity();
-        GLU.gluOrtho2D(gl, 0, w/65536.0f, h/65536.0f, 0);
+        GLU.gluOrtho2D(gl, 0, w, h, 0);
 
-        gl.glVertexPointer(2, GL10.GL_FIXED, 0, v);
+        gl.glVertexPointer(2, GL10.GL_BYTE, 0, v);
         gl.glDrawArrays(GL10.GL_TRIANGLE_FAN, 0, 4);
     }
 
     public static void drawScreenSpaceTexture(Texture2D tex) {
         tex.makeCurrent();
-        tex.gl.glTexCoordPointer(2, GL10.GL_FIXED, 0, v);
+        tex.gl.glTexCoordPointer(2, GL10.GL_BYTE, 0, v);
 
         drawScreenSpaceQuad(tex.gl);
     }
